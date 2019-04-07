de371e659936a95a5e47251ff67c8c30eb5cdab7
Latcho <latcho.nikolov@gmail.com>
2019-04-07 13:17:10 +0300

    added service layer with service.base.UserServiceBase interface

--- src/main/java/com/fmob/webcrawler/repositories/UserRepository.java
+++ src/main/java/com/fmob/webcrawler/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.fmob.webcrawler.repositories;
 
 import com.fmob.webcrawler.models.User;
 import com.fmob.webcrawler.repositories.base.UserRepositoryBase;
-import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
---
+++ src/main/java/com/fmob/webcrawler/service/base/UserServiceBase.java
@@ -0,0 +1,14 @@
+package com.fmob.webcrawler.service.base;
+
+import com.fmob.webcrawler.models.User;
+
+import java.util.List;
+
+public interface UserServiceBase {
+
+    String saveNew(User user);
+
+    List<User> getAllByOriginAndDestination(String origin, String destination);
+
+    void confirmUser(User user);
+}
