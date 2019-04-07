3a19673d6fa224293d4fae3b3e1b043c218f96a2
Latcho <latcho.nikolov@gmail.com>
2019-04-07 20:51:21 +0300

    added some methods in FlightRepositoryBase interface

--- src/main/java/com/fmob/webcrawler/repositories/base/FlightRepositoryBase.java
+++ src/main/java/com/fmob/webcrawler/repositories/base/FlightRepositoryBase.java
@@ -1,4 +1,18 @@
 package com.fmob.webcrawler.repositories.base;
 
-public class FlightRepositoryBase<T> {
+import java.util.List;
+
+public interface FlightRepositoryBase<T> {
+
+    String save(T flight);
+
+    List<T> getAll();
+
+    List<T> getAllByOriginAndDestination(String origin, String destination);
+
+    List<T> getAllByPrice(double price);
+
+    T getByLowestPriceForOriginAndDestination(String origin, String destination);
+
+    
 }
