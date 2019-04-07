f8fe46708c583a0a2c137b903f10a8df016f679c
Latcho <latcho.nikolov@gmail.com>
2019-04-07 12:22:21 +0300

    changed models.User class to Entity and added fields without frequencyID

--- src/main/java/com/fmob/webcrawler/models/User.java
+++ src/main/java/com/fmob/webcrawler/models/User.java
@@ -1,4 +1,56 @@
 package com.fmob.webcrawler.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "userId")
+})
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private int userID;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "origin")
+    private String origin;
+
+    @Column(name = "destination")
+    private String destination;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
 }
