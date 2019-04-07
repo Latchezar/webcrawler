225d28b0e8eef38a0fe44ede9282f5758546bcb1
Latcho <latcho.nikolov@gmail.com>
2019-04-07 20:23:42 +0300

    added FrequencyServiceBase interface

---
+++ src/main/java/com/fmob/webcrawler/service/base/FrequencyServiceBase.java
@@ -0,0 +1,14 @@
+package com.fmob.webcrawler.service.base;
+
+import com.fmob.webcrawler.models.Frequency;
+
+import java.util.List;
+
+public interface FrequencyServiceBase {
+
+    List<Frequency> getAll();
+
+    Frequency getById(int id);
+
+    Frequency getByName(int id);
+}
