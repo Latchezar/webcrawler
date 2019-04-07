db13a09f582f77230ac2d2d5408f59b5413fe293
Latcho <latcho.nikolov@gmail.com>
2019-04-07 21:32:33 +0300

    added get and post methods for saving and getting flights in flight controllers

--- src/main/java/com/fmob/webcrawler/controller/rest/FlightController.java
+++ src/main/java/com/fmob/webcrawler/controller/rest/FlightController.java
@@ -1,9 +1,12 @@
 package com.fmob.webcrawler.controller.rest;
 
+import com.fmob.webcrawler.models.Flight;
 import com.fmob.webcrawler.service.base.FlightServiceBase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/flight")
@@ -14,5 +17,24 @@ public class FlightController {
     public FlightController(FlightServiceBase flightService){
         this.flightService = flightService;
     }
-    
+
+    @PostMapping("/new")
+    public String save(@RequestBody Flight flight){
+        return this.flightService.save(flight);
+    }
+
+    @GetMapping("/all")
+    public List<Flight> getAll(){
+        return this.flightService.getAll();
+    }
+
+    @GetMapping("/specific")
+    public List<Flight> getAllForOriginAndDestination(@PathParam("origin")String origin, @PathParam("destination")String destination){
+        return this.flightService.getAllByOriginAndDestination(origin, destination);
+    }
+
+    @GetMapping("/lowest")
+    public Flight getLowestForOriginAndDestination(@PathParam("origin") String origin, @PathParam("destination") String destination){
+        return this.flightService.getByLowestPriceForOriginAndDestination(origin, destination);
+    }
 }
