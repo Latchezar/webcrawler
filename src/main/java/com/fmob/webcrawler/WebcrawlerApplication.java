e43708611e2d421b65241bbf4711468ea825109d
Latcho <latcho.nikolov@gmail.com>
2019-04-07 12:16:50 +0300

    created hibernate configuration file and added session factory bean in webvrawler application class

--- src/main/java/com/fmob/webcrawler/WebcrawlerApplication.java
+++ src/main/java/com/fmob/webcrawler/WebcrawlerApplication.java
@@ -1,13 +1,23 @@
 package com.fmob.webcrawler;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebcrawlerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebcrawlerApplication.class, args);
+    }
+
+    @Bean
+    public SessionFactory sessionFactory(){
+        return new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
     }
 
 }
