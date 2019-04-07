00f68df762b75dea61588775214739299ec6e643
Latcho <latcho.nikolov@gmail.com>
2019-04-07 12:33:32 +0300

    added anotated classes in session factory bean in webcrawler application

--- src/main/java/com/fmob/webcrawler/WebcrawlerApplication.java
+++ src/main/java/com/fmob/webcrawler/WebcrawlerApplication.java
@@ -1,5 +1,8 @@
 package com.fmob.webcrawler;
 
+import com.fmob.webcrawler.models.Flight;
+import com.fmob.webcrawler.models.Frequency;
+import com.fmob.webcrawler.models.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +20,9 @@ public class WebcrawlerApplication {
     public SessionFactory sessionFactory(){
         return new Configuration()
                 .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Flight.class)
+                .addAnnotatedClass(Frequency.class)
                 .buildSessionFactory();
     }
-
 }
