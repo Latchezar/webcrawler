7d40f77082783ae390d2c968caaee3b3f139cc3c
Latcho <latcho.nikolov@gmail.com>
2019-04-08 10:47:56 +0300

    now when an email is sent with an offer it is also written in the database

--- src/main/java/com/fmob/webcrawler/util/EmailService.java
+++ src/main/java/com/fmob/webcrawler/util/EmailService.java
@@ -1,26 +1,38 @@
 package com.fmob.webcrawler.util;
 
+import com.fmob.webcrawler.models.SentEmail;
+import com.fmob.webcrawler.models.User;
+import com.fmob.webcrawler.repositories.base.SentEmailRepositoryBase;
 import com.fmob.webcrawler.util.base.EmailServiceBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class EmailService implements EmailServiceBase {
     private JavaMailSender emailSender;
+    private SentEmailRepositoryBase<SentEmail> emailRepository;
 
     @Autowired
-    public EmailService(JavaMailSender emailSender) {
+    public EmailService(JavaMailSender emailSender, SentEmailRepositoryBase<SentEmail> emailRepository) {
         this.emailSender = emailSender;
+        this.emailRepository = emailRepository;
     }
 
     @Override
-    public void sendEmail(String to, String subject, String text) {
+    public void sendEmail(User to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
+        message.setTo(to.getEmail());
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+        SentEmail email = new SentEmail();
+        email.setEmailText(text);
+        email.setTimestam(new Date().getTime());
+        email.setUserId(to.getUserID());
+        this.emailRepository.saveEmail(email);
     }
 }
--- src/main/java/com/fmob/webcrawler/util/base/EmailServiceBase.java
+++ src/main/java/com/fmob/webcrawler/util/base/EmailServiceBase.java
@@ -1,6 +1,8 @@
 package com.fmob.webcrawler.util.base;
 
+import com.fmob.webcrawler.models.User;
+
 public interface EmailServiceBase {
 
-    void sendEmail(String to, String subject, String text);
+    void sendEmail(User to, String subject, String text);
 }
