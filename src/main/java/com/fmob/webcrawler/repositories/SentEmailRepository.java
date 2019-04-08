337698a6b81e86a9e71d422e0ccfb90b9ad73a39
Latcho <latcho.nikolov@gmail.com>
2019-04-08 10:43:45 +0300

    added implementation on SentEmailRepository

--- src/main/java/com/fmob/webcrawler/repositories/SentEmailRepository.java
+++ src/main/java/com/fmob/webcrawler/repositories/SentEmailRepository.java
@@ -1,6 +1,9 @@
 package com.fmob.webcrawler.repositories;
 
+import com.fmob.webcrawler.models.SentEmail;
 import com.fmob.webcrawler.repositories.base.SentEmailRepositoryBase;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,6 +19,19 @@ public class SentEmailRepository<T> implements SentEmailRepositoryBase<T> {
 
     @Override
     public void saveEmail(T email) {
-        
+        if (email instanceof SentEmail) {
+            SentEmail emailToSave = (SentEmail) email;
+            Session session = sessionFactory.openSession();
+            try {
+                session.beginTransaction();
+                session.save(emailToSave);
+                session.getTransaction().commit();
+                session.close();
+            } catch (HibernateException e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            System.out.println("Incompatible object: not SentEmail.class");
+        }
     }
 }
